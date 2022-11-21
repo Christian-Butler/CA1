73bf044afeb3083adbec7f0cc70044727686bb79
Christian Butler <N00211989@iadt.ie>
2022-11-21 16:56:05 +0000

    hlh

---
+++ database/seeders/PlayerSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Player;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+
+class PlayerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Player::factory()->times(50)->create();
+    }
+}
