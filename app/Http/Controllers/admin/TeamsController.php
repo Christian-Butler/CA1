f1b1dbf9ae5df9b95b90b911482fbd9998591359
Christian Butler <N00211989@iadt.ie>
2022-12-10 22:33:49 +0000

    created the teams controller to view show edit create and delete teams

---
+++ app/Http/Controllers/admin/TeamsController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Team;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TeamsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = Auth::user();
+        $user->authorizeRoles('admin');
+
+        $teams = Team::all();
+
+        return view('admin.teams.index')->with('teams', $teams);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = Auth::user();
+        $user->authorizeRoles('admin');
+
+        if(!Auth::id()) {
+            return abort(403);
+        }
+
+        $team = Team::where('id', $id)->firstOrFail();
+        // if($player->user_id != Auth::user()){
+        //     return abort(403);
+        // }
+        return view('admin.teams.show')->with('team', $teams);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
